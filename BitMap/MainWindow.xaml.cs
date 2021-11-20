69c8c9a5b11524f23dd3b4560581b0e4d27d758a
UserName <Email>
2021-11-19 22:31:42 -0500

    Move ball behavior to VM

--- BitMap/MainWindow.xaml.cs
+++ BitMap/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -11,7 +12,7 @@ namespace BitMap
     /// </summary>
     public partial class MainWindow : Window
     {
-        public WriteableBitmap WB { get; set; }
+        public WriteableBitmap WB => _vm.WB;
         private Random _rand;
         private MainWindowViewModel _vm;
         public MainWindow()
@@ -19,13 +20,12 @@ namespace BitMap
 
             InitializeComponent();
             _vm = DataContext as MainWindowViewModel;
-
+            if (_vm is null) throw new NullReferenceException("VM");
 
             _rand = new();
-            WB = new(300, 300, 96, 96, PixelFormats.Bgra32, null);
-            //img.Source = WB;
+            img.Source = _vm.WB;
             DispatcherTimer renderTimer = new(DispatcherPriority.Render);
-            renderTimer.Interval = TimeSpan.FromMilliseconds(1);
+            renderTimer.Interval = TimeSpan.FromMilliseconds(10);
             renderTimer.Tick += RenderTimer_Tick;
             renderTimer.Start();
         }
@@ -33,17 +33,33 @@ namespace BitMap
         private void RenderTimer_Tick(object? sender, EventArgs e)
         {
             WB.Lock();
-
+            _vm.Tick();
             byte[] colorData = { (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), 255 };
-
+            byte[] black = { 0, 0, 0, 255 };
+            var stride = WB.PixelWidth * WB.Format.BitsPerPixel / 8;
             for (int x = 0; x < WB.PixelWidth; x++)
             {
-
                 for (int y = 0; y < WB.PixelWidth; y++)
                 {
                     Int32Rect rect = new(x, y, 1, 1);
-                    var stride = WB.PixelWidth * WB.Format.BitsPerPixel / 8;
-                    WB.WritePixels(rect, colorData, stride, 0);
+                    bool Any = false;
+                    foreach (var ball in _vm.Balls)
+                    {
+                        if (Vector2.DistanceSquared(new(x, y), ball.Location) < 10)
+                        {
+                            Any = true;
+                            WB.WritePixels(rect, black, stride, 0);
+                            break;
+                        }
+                    }
+                    if (!Any)
+                    {
+                        WB.WritePixels(rect, colorData, stride, 0);
+                    }
+                    foreach (var ball in _vm.Balls)
+                    {
+                        //WB.FillEllipseCentered((int)ball.Location.X, (int)ball.Location.Y, 5, 5, Colors.Blue); //Extremely slow.
+                    }
                 }
             }
             WB.Unlock();
--- BitMap/MainWindowViewModel.cs
+++ BitMap/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -11,11 +12,41 @@ namespace BitMap
     public class MainWindowViewModel
     {
         public WriteableBitmap WB { get; set; } = new(300, 300, 96, 96, PixelFormats.Bgra32, null);
-
+        public List<Ball> Balls { get; set; } = new();
+        private Random _rand;
         public MainWindowViewModel()
         {
-
-
+            _rand = new();
         }
+
+        public void Tick()
+        {
+            if (_rand.NextDouble() > 0.75)
+            {
+                var x = _rand.Next(0, 300);
+                var y = _rand.Next(0, 300);
+                var rotation = (float)Math.Atan2(150 - y, 150 - x);
+                Balls.Add(new() { Location = new(x, y), Rotation = rotation });
+            }
+
+            for (var index = 0; index < Balls.Count; index++)
+            {
+                var ball = Balls[index];
+                var vector = new Vector2((float)Math.Cos(ball.Rotation), (float)Math.Sin(ball.Rotation));
+                ball.Location += 10 * vector;
+                if (ball.Location.X < 0 || ball.Location.X > 300 || ball.Location.Y < 0 || ball.Location.Y > 300)
+                {
+                    Balls.RemoveAt(index);
+                }
+            }
+        }
+
     }
+
+    public class Ball
+    {
+        public Vector2 Location { get; set; }
+        public float Rotation { get; set; }
+    }
+
 }
