fddc3642e98975acd6f8b24a8bc44aa1a67e01c6
UserName <Email>
2021-11-19 21:40:57 -0500

    Add project files.

---
+++ BitMap/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace BitMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WriteableBitmap WB { get; set; }
+        private Random _rand;
+        private MainWindowViewModel _vm;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            _vm = DataContext as MainWindowViewModel;
+
+
+            _rand = new();
+            WB = new(300, 300, 96, 96, PixelFormats.Bgra32, null);
+            //img.Source = WB;
+            DispatcherTimer renderTimer = new(DispatcherPriority.Render);
+            renderTimer.Interval = TimeSpan.FromMilliseconds(1);
+            renderTimer.Tick += RenderTimer_Tick;
+            renderTimer.Start();
+        }
+
+        private void RenderTimer_Tick(object? sender, EventArgs e)
+        {
+            WB.Lock();
+
+            byte[] colorData = { (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), 255 };
+
+            for (int x = 0; x < WB.PixelWidth; x++)
+            {
+
+                for (int y = 0; y < WB.PixelWidth; y++)
+                {
+                    Int32Rect rect = new(x, y, 1, 1);
+                    var stride = WB.PixelWidth * WB.Format.BitsPerPixel / 8;
+                    WB.WritePixels(rect, colorData, stride, 0);
+                }
+            }
+            WB.Unlock();
+        }
+    }
+}
---
+++ BitMap/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BitMap
+{
+    public class MainWindowViewModel
+    {
+        public WriteableBitmap WB { get; set; } = new(300, 300, 96, 96, PixelFormats.Bgra32, null);
+
+        public MainWindowViewModel()
+        {
+
+
+        }
+    }
+}
